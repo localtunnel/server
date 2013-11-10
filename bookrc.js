ac1fbc740ede0d4a25270e0e182ffb6899d3441b
Roman Shtylman <shtylman@gmail.com>
2013-11-10 00:19:00 -0500

    continue to handle uncaught exception until exit

--- bookrc.js
+++ bookrc.js
@@ -5,7 +5,7 @@ require('superstack');
 log.use(require('book-git')(__dirname));
 log.use(require('book-raven')(process.env.SENTRY_DSN));
 
-process.once('uncaughtException', function(err) {
+process.on('uncaughtException', function(err) {
     log.panic(err);
     setTimeout(process.exit.bind(process, 1), 2000);
 });
