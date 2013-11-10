1fe63831d831c6dc7690f78e822f06e6e15bb6d7
Roman Shtylman <shtylman@gmail.com>
2013-11-10 00:19:55 -0500

    remove error log from socket errors
    We don't care, just close the socket

--- proxy.js
+++ proxy.js
@@ -95,7 +95,6 @@ var Proxy = function(opt, cb) {
 
         // close will be emitted after this
         socket.on('error', function(err) {
-            log.error(err);
             socket.end();
         });
 
