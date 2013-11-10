2bbf98c24c58d7961592f6e7b3277c381349514f
Roman Shtylman <shtylman@gmail.com>
2013-11-10 00:13:42 -0500

    add superstack for better stacktraces

--- bookrc.js
+++ bookrc.js
@@ -1,5 +1,6 @@
 /// bookrc logging setup
 var log = require('book').default();
+require('superstack');
 
 log.use(require('book-git')(__dirname));
 log.use(require('book-raven')(process.env.SENTRY_DSN));
