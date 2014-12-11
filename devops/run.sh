57ebcc90e1ff90f7e2bb179b0f169e7f50cb084b
Roman Shtylman <shtylman@gmail.com>
2014-12-11 10:54:53 -0800

    add devops folder to show how to easily run on a server

---
+++ devops/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run --restart always --name localtunnel -d --net host defunctzombie/localtunnel-server:0.0.5 bin/server --secure --port 2000
+
+docker run --restart always --name nginx -d --net host -v /home/core/nginx/nginx.conf:/etc/nginx/nginx.conf -v /home/core/nginx/sites:/etc/nginx/sites -v /home/core/nginx/ssl:/etc/nginx/ssl nginx:1.7.8
