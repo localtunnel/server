cdfd9ceb9fe93560d0fe31b054ced4c398b5063a
Manuel <github@manuelbieh.de>
2016-05-10 02:43:50 +0200

    allow recursive subdomains

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ module.exports = function(opt) {
         var req_id = req.params.req_id;
 
         // limit requested hostnames to 63 characters
-        if (! /^[a-z0-9]{4,63}$/.test(req_id)) {
+        if (! /^[a-z0-9\.]{4,63}$/.test(req_id)) {
             var err = new Error('Invalid subdomain. Subdomains must be lowercase and between 4 and 63 alphanumeric characters.');
             err.statusCode = 403;
             return next(err);
